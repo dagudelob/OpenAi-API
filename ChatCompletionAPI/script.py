afab00e531df559ff40321af3f071307eeb35729
David Agudelo <agudelo.david@hotmail.com>
2025-05-03 10:55:31 -0400

    borro archivo script y agrego archivo "tio hola"

--- ChatCompletionAPI/script.py
+++
@@ -1,6 +0,0 @@
-""" Clase para el uso de Assistants (Beta)
-En esta clase hemos creado un asistente, y luego accedido a él para enviarle un mensaje a través de un hilo.
-Después hemos ejecutado el asistente y hemos obtenido los pasos que ha realizado para responder a nuestro mensaje (incluyendo el uso de la herramienta Code Interpreter).
-Finalmente, hemos obtenido la respuesta del asistente y hemos imprimido los pasos que ha realizado para responder a nuestro mensaje.  """
-
-import asdfsadfasdfasdf
